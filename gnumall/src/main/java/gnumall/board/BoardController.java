94d31c2d0ecf521e0e5c54e35da89e1297001463
subinmun1997 <qzxy812@gmail.com>
2020-11-16 18:49:33 +0900

    게시판 뷰페이지

--- gnumall/src/main/java/gnumall/board/BoardController.java
+++ gnumall/src/main/java/gnumall/board/BoardController.java
@@ -1,15 +1,21 @@
 package gnumall.board;
 
+import javax.inject.Inject;
 
-import gnumall.common.util.DataMap;
-import gnumall.common.util.HttpUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import gnumall.board.BoardController;
+import gnumall.board.service.BoardService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
+import gnumall.board.BoardMapper;
 import gnumall.board.model.BoardVO;
 import gnumall.board.service.BoardService;
 
@@ -19,33 +25,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 @Controller
-//@RequestMapping(value="/board")
 public class BoardController {
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
-	private final BoardService boardService;
-	
-	public BoardController(BoardService boardService) {
-		this.boardService = boardService;
-	}
-	
-	@RequestMapping(value = "/board", method = {RequestMethod.GET})
-	public String getPostTable(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		//model.addAttribute("boardList",boardService.getBoardList());
-		DataMap paramMap = HttpUtil.getRequestDataMap(request);
-		logger.info("Hello Board");
-		logger.info("session ID : " + request.getRequestedSessionId());
-		logger.info("{}",paramMap);
-		//logger.info("request : "+ request + "response : "+ response);
-		
-		try {
-			logger.info("getBoardList 쿼리 실행 결과: " + boardService.getBoardList());
-		} catch(Exception e) {
-			logger.error("게시물 조회 오류 : {}",e);
-		}
-		
-		return "/content/board";
-	}
 
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+    @Autowired
+    private BoardMapper mapper;
+
+    @Inject
+    public BoardController(BoardMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @RequestMapping(value = "/board", method = RequestMethod.GET)
+    public String getBoardList(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        logger.info("getBoardList...");
+        
+        List<BoardVO> boardList = mapper.getBoardList();
+        try {
+			boardList = mapper.getBoardList();
+			logger.info("getBoardList 쿼리 실행 결과 : ");
+			boardList.forEach((temp) -> {
+				logger.info("{}", temp);
+			});
+		} catch (Exception e) {
+			logger.error("게시물 조회 오류 : {}", e);
+		}
+        model.addAttribute("list",boardList);
+        
+		
+        return "/content/board";
+    }
 }
